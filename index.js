8cc6e2d0ad8f2d228e5f8d119692144a536b281c
jaeyunkim <kjy_1998@naver.com>
2021-03-07 21:50:03 +0900

    20210307

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 const express = require('express');
 // 익스프레스 함수를 사용하겠다
 const app = express();
-// 포트는 3002을 사용
+// 포트는 3005을 사용
 const port = 3005;
 // 바디파서를 사용하겠다
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const config = require('./config/key');
-const { User } = require("./models/User");
+const { User } = require('./models/User');
 
 // bodyParser가 application/x-www-form-urlencoded 데이터를 분석해서 가져옴.
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,18 +17,20 @@ app.use(bodyParser.json());
 // 몽구스 모듈을 사용하겠다
 const mongoose = require('mongoose');
 // 이 저장소를 연결하겠다, 옵션을 추가하겠다
+// 파일을 배포했을 때 mongoURI까지 배포되지 않도록 변수를 받아옴
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('오늘도 모험을 떠나요.'));
 
+// register router
 app.post('/register', (req, res) => {
     // 회원가입 할 때 필요한 정보들을 client에서 가져오면
     const user = new User(req.body);
-    // 몽고DB에 담는다
+    // 몽고DB에 담는다(save)
     user.save((err, userInfo) => {
-        if(err) return res.json({success: false, err});
+        if (err) return res.json({success: false, err});
         return res.status(200).json({success:true});
     });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+// 비밀번호 암호화 라이브러리 사용
+const bcrypt = require('bcrypt');
+// 암호화에 필요한 salt를 10글자로 만들겠다.
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,7 +39,30 @@ const userSchema = mongoose.Schema({
     }
 });
 
-// User 변수에 'User'라는 모델 안의 'userSchema'라는 스키마를 담는다.
+// mongoose 메소드로 'save'를 실행하기 전에(pre) 어떤 행동을 하겠다.
+userSchema.pre('save', function (next) {
+    var user = this;
+    // password가 변경될 때만 암호화 시킨다
+    if (user.isModified('password')) {
+        // salt를 생성(gen), 이용해서 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            // user.password => 암호화 전 값
+            // hash => 암호화 이후 값
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+        // passwrod가 변경되지 않을 때는 바로 'save'를 실행하겠다
+    } else {
+        next();
+    }
+});
+// User 변수에 'User'라는 모델 안의 userSchema라는 스키마를 담는다.
 const User = mongoose.model('User', userSchema);
 // 외부파일에서 User 모델사용가능하게
-module.exports = { User }
+module.exports = {
+    User
+}
